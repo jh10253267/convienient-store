7b64f0a36488945ebe12568c6dfbc95e7fec7469
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-05-07 15:43:23 +0900

    init commit

---
+++ src/main/java/com/example/convenientstore/ConvenientStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.convenientstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenientStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvenientStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/convenientstore/ConvenientStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convenientstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenientStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
